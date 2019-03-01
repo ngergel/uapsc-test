5068c0842136dfb06641fbe016d44c0111b135de
Noah Gergel <n.gergel1@gmail.com>
2019-03-01 08:43:01 -0700

    Refactored the script to accept any  order of arguments.

--- uatest.sh
+++ uatest.sh
@@ -12,6 +12,9 @@ ARGS=("$@")
 # Argument iterator.
 i="0"
 
+# All possible flag variables.
+# oflag, hflag, cmnd
+
 # Flags to compile C/C++ programs with.
 CFLAGS="-g -O2 -static -std=gnu++17 -Wall"
 
@@ -20,65 +23,75 @@ CFLAGS="-g -O2 -static -std=gnu++17 -Wall"
 
 # Prints correct usage. Takes in an argument as the error message.
 usage() {
-	echo -e "\033[31mError\033[m: Please specify a $1."
+	echo -e "\033[31mError\033[m: $1"
 	echo -e "\033[33mUsage\033[m: ./uatest.sh [options] <command> [-p problem] [file-name]"
 	exit 2
 }
 
-# Verify a command or option is given.
-test $# -eq 0 && usage command
+# Verify a command argument is given.
+test $# -eq 0 && usage "No command given."
+
+# Parse every argument.
+while [ $i -lt $# ]
+do
+	case ${ARGS[$(( i++ ))]} in
+		-h|--help|help) # Help option.
+			hflag="1" ;;
+		-o|--output) # Output option.
+			oflag="1" ;;
+		-p) # Problem id argument.
+			test $i -lt $# && prob="${ARGS[$(( i++ ))]}" || usage "Missing problem id." ;;
+		clean) # Clean command.
+			test -z $cmnd && cmnd="clean" ;;
+		test) # Test command.
+			test -z $cmnd && cmnd="test" ;;
+		submit) # Submit command.
+			test -z $cmnd && cmnd="submit" ;;
+		*) # General catch for file or invalid arguments.
+			if [ -z $file ]
+			then
+				file="${ARGS[$i - 1]}"
+				test -z $prob && prob="${file##*/}" && prob="${prob%.*}"
+			else
+				usage "Invalid argument, '${ARGS[$i - 1]}'."
+			fi
+			;;
+	esac
+done
+
+# Get problem id and file from config if possible.
+test -f $CONFIG && . $CONFIG
+test -z $prob && prob="$CPROB"
+test -z $file && file="$CFILE"
 
 # Help option.
-if [ $1 = "-h" ] || [ $1 = "--help" ] || [ $1 = "help" ]
-then
-	cat $SOURCE_PATH/src/help.txt && exit 0 || exit 1
-fi
+test $hflag && cat $SOURCE_PATH/src/help.txt && exit 0
+
+# Make sure a command was actually given in the arguments.
+test -z $cmnd && usage "No valid command given."
 
 # Clean command.
-test $1 = "clean" && rm -rf $DIR && exit 0
-
-# Check for options.
-test $i -lt $# && test ${ARGS[$i]} = "-o" || test ${ARGS[$i]} = "--output" && output_flag="1" && (( i++ ))
-
-# Verify a command is given after the options.
-test ! $i -lt $# && usage command
+test $cmnd = "clean" && rm -rf $DIR && exit 0
 
 # Test command.
-if [ ${ARGS[$i]} = "test" ]
+if [ $cmnd = "test" ]
 then
-	(( i++ ))
-
-	# Get problem id from file if possible.
-	test -f $CONFIG && . $CONFIG
-	
-	# Read in problem id and file arguments.
-	if [ $((i + 2)) -lt $# ] && [ ${ARGS[$i]} = "-p" ]
-	then
-		# If both problem id and file name were given.
-		new_prob="${ARGS[$(( ++i ))]}"
-		new_file="${ARGS[$(( ++i ))]}"
-	elif [ $i -lt $# ] && [ ${ARGS[$i]} != "-p" ]
-	then
-		# Read in file name if it was given and implicitly infer problem id.
-		new_file="${ARGS[$i]}"
-		new_prob="${ARGS[$i]##*/}" && new_prob="${new_prob%.*}"
-	fi
+	# Check if problem id or file has been given.
+	test -z $prob$file && usage "Please specify a problem id and file."
+	test -z $file && usage "Please specify a file."
 
 	# If arguments differ from config data, download new test cases.
-	if [ $new_prob ] && { [ -z $PROB ] || [ $new_prob != $PROB ]; }
+	if [ -z $CPROB ] || [ $prob != $CPROB ]
 	then
-		# Information for the user.
-		PROB="$new_prob" && FILE="$new_file"
+		# Information for the user and download sample test cases.
 		printf "Downloading test cases..."
-
-		# Download sample test cases.
 		mkdir -p $DIR
-		wget -q -O $DIR/samples.zip https://open.kattis.com/problems/$PROB/file/statement/samples.zip > /dev/null
+		wget -q -O $DIR/samples.zip https://open.kattis.com/problems/$prob/file/statement/samples.zip > /dev/null
 
 		# Catch when not downloadable.
 		if [ $? -ne 0 ]; then
 			echo "unable to download test cases."
-			echo -e "Problem id: $PROB\nFile: $FILE\n"
+			echo -e "Problem id: $prob\nFile: $file\n"
 			rm -f $DIR/samples.zip
 			exit 1
 		else
@@ -87,33 +100,24 @@ then
 			unzip -q $DIR/samples.zip -d $DIR
 			rm -f $DIR/samples.zip
 		fi
-	elif [ $new_file ]
-	then
-		# Update file if it has changed but the problem has not.
-		FILE="$new_file"
 	fi
 
-	# Check if problem id or file has been given.
-	test -z $PROB$FILE && usage "problem id and file"
-	test -z $PROB && usage "problem id"
-	test -z $FILE && usage file
-
 	# Catch non-supported languages.
-	if [[ $FILE != *".c" ]] && [[ $FILE != *".cpp" ]] && [[ $FILE != *".py" ]]
+	if [[ $file != *".c" ]] && [[ $file != *".cpp" ]] && [[ $file != *".py" ]]
 	then
 		echo "This script only supports C, C++, and Python 3."
 		exit 2
 	fi
 
 	# Update config file and print problem id and file name for the user.
-	echo -e "PROB=$PROB\nFILE=$FILE" > $CONFIG
-	echo -e "Problem id: $PROB\nFile: $FILE\n"
+	echo -e "CPROB=$prob\nCFILE=$file" > $CONFIG
+	echo -e "Problem id: $prob\nFile: $file\n"
 
 	# Compile C or C++.
-	if [[ $FILE == *".c" ]] || [[ $FILE == *".cpp" ]]
+	if [[ $file == *".c" ]] || [[ $file == *".cpp" ]]
 	then
 		# Compile the program.
-		g++ $FILE $CFLAGS -o $DIR/a.out
+		g++ $file $CFLAGS -o $DIR/a.out
 		
 		# Exit on a compile error.
 		if [ $? -ne 0 ]
@@ -127,18 +131,18 @@ then
 	for i in $DIR/*.in
 	do
 		# Run the program against the test case.
-		if [[ $FILE == *".c" ]] || [[ $FILE == *".cpp" ]]
+		if [[ $file == *".c" ]] || [[ $file == *".cpp" ]]
 		then
-			START=$(date +%s%3N)
+			start=$(date +%s%3N)
 			$DIR/a.out < $i > ${i%.*}.test
-			COMP=$?
-			TIME=$(($(date +%s%3N)-START))
-		elif [[ $FILE == *".py" ]]
+			comp=$?
+			time=$(($(date +%s%3N)-start))
+		elif [[ $file == *".py" ]]
 		then
-			START=$(date +%s%3N)
-			python3 $FILE < $i > ${i%.*}.test
-			COMP=$?
-			TIME=$(($(date +%s%3N)-START))
+			start=$(date +%s%3N)
+			python3 $file < $i > ${i%.*}.test
+			comp=$?
+			time=$(($(date +%s%3N)-start))
 		else
 			echo "This script only supports C, C++, and Python 3."
 			exit 2
@@ -147,16 +151,16 @@ then
 		# See what the result was.
 		if diff ${i%.*}.test ${i%.*}.ans > /dev/null
 		then
-			echo -e "${i##*/}: \033[32mCorrect Answer\033[m...${TIME}ms"
-		elif [ $COMP -eq 0 ]
+			echo -e "${i##*/}: \033[32mCorrect Answer\033[m...${time}ms"
+		elif [ $comp -eq 0 ]
 		then
-			echo -e "${i##*/}: \033[31mWrong Answer\033[m...${TIME}ms"
+			echo -e "${i##*/}: \033[31mWrong Answer\033[m...${time}ms"
 		else
-			echo -e "${i##*/}: \033[31mRuntime Error\033[m...${TIME}ms\n"
+			echo -e "${i##*/}: \033[31mRuntime Error\033[m...${time}ms\n"
 		fi
 
 		# If the output flag was set, print the output.
-		if [ $output_flag ] && [ $COMP -eq 0 ]
+		if [ $oflag ] && [ $comp -eq 0 ]
 		then
 			cat ${i%.*}.test && echo
 		fi
@@ -171,36 +175,17 @@ then
 fi
 
 # Submit command.
-if [ ${ARGS[$i]} = "submit" ]
+if [ $cmnd = "submit" ]
 then
-	(( i++ ))
-
-	# Get problem id from file if possible.
-	test -f $CONFIG && . $CONFIG
-	
-	# Read in problem id and file arguments.
-	if [ $((i + 2)) -lt $# ] && [ ${ARGS[$i]} = "-p" ]
-	then
-		# If both problem id and file name were given.
-		PROB="${ARGS[$(( ++i ))]}"
-		FILE="${ARGS[$(( ++i ))]}"
-	elif [ $i -lt $# ] && [ ${ARGS[$i]} != "-p" ]
-	then
-		# Read in file name if it was given and implicitly infer problem id.
-		FILE="${ARGS[$i]}"
-		PROB="${ARGS[$i]##*/}" && PROB="${PROB%.*}"
-	fi
-
 	# Check if problem id or file has been given.
-	test -z $PROB$FILE && usage "problem id and file"
-	test -z $PROB && usage "problem id"
-	test -z $FILE && usage file
+	test -z $prob$file && usage "Please specify a problem id and file."
+	test -z $file && usage "Please specify a file."
 
 	# Tells the user what problem they are testing incase of issues.
-	echo -e "Problem id: $PROB\nFile: $FILE\n"
+	echo -e "Problem id: $prob\nFile: $file\n"
 
 	# Submit the file to Kattis, clean, and exit.
-	python3 $SOURCE_PATH/src/submit.py -p $PROB $FILE -f
+	python3 $SOURCE_PATH/src/submit.py -p $prob $file -f
 	rm -rf $DIR
 	exit 0
 fi
